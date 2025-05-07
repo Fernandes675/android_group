deab777e41acaef4a00a840773de63f5c8ebfeef
ViniciusG <viniciusgscontatos@gmail.com>
2025-05-07 20:41:42 -0300

    Password

---
+++ constants/colors.ts
@@ -0,0 +1,7 @@
+export default {
+  zinc: '0c0c1b',
+  black:'#000',
+  white: '#FFF',
+  green: '#6A9C2E',
+  gray: '#F5F5F5',
+};
---
+++ src/app/index.tsx
@@ -0,0 +1,141 @@
+import { View, StyleSheet, Image, TextInput, Pressable, Text, ScrollView, Alert } from 'react-native';
+import colors from '@/constants/colors';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import { supabase } from '../lib/supabase';
+import { router } from 'expo-router'
+
+export default function Login() {
+
+    const [email, setEmail] = useState ('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSignIn(){     
+        setLoading(true);
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+
+        if(error){
+            Alert.alert('Error', error.message)
+            setLoading(false);
+            return;
+        }
+
+        setLoading(false);
+        router.replace('/(panel)/profile/page')
+
+    }
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.container}>
+                <Image
+                    source={require('../../assets/images/logo.png')}
+                    style={styles.logo}
+                    resizeMode="contain"
+                />
+
+                <View style={styles.form}>
+                    <Text style={styles.label}>Email</Text>
+                    <TextInput
+                        placeholder="Digite seu e-mail..."
+                        style={styles.input}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+
+                    <Text style={styles.label}>Senha</Text>
+                    <TextInput
+                        placeholder="Digite sua senha..."
+                        style={styles.input}
+                        secureTextEntry
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+
+                    <Pressable style={styles.button} onPress={handleSignIn}>
+                        <Text style={styles.buttonText}>
+                            {loading ? 'Carregando...' : 'Entrar'}
+                            </Text>
+                    </Pressable>
+
+                    <Link href='/(auth)/signup/page' style={styles.link}>
+                        <Text>Ainda não possui uma conta? Cadastre-se</Text>    
+                    </Link>
+                    <Link href="/(auth)/forgot-password/page" style={styles.forgotLink}>
+                        <Text>Esqueci minha senha</Text>
+                    </Link>
+
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        backgroundColor: colors.white,
+    },
+    container: {
+        alignItems: 'center',
+        paddingVertical: 40,
+    },
+    logo: {
+        width: 250,
+        height: 160,
+        marginBottom: 30,
+        marginTop: -25,
+    },
+    form: {
+        width: '85%',
+        backgroundColor: colors.gray,
+        borderRadius: 16,
+        padding: 20,
+        alignItems: 'center',
+    },
+    label: {
+        alignSelf: 'flex-start',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: colors.black,
+        marginBottom: 6,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        backgroundColor: colors.white,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        marginBottom: 16,
+    },
+    button: {
+        backgroundColor: colors.green,
+        width: '100%',
+        paddingVertical: 12,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 16,
+        textAlign: 'center',
+
+
+    },
+    forgotLink: {
+        marginTop: 12,
+        textAlign: 'center',
+    }
+      
+});
